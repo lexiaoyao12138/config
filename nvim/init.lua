098dfc03ffdd63e3236561604cc16ba55cc4d41b
lexiaoyao12138 <1639503985@qq.com>
2022-08-07 22:59:13 +0800

    upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,384 @@
 -- init.lua
+vim.notify = require("notify")
+
+vim.opt.number = true
+vim.opt.wrap = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.hls = true
+vim.opt.tabstop=4
+vim.opt.shiftwidth=4
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.filetype = off
+
+vim.api.nvim_command(':set bg=dark')
+vim.api.nvim_command(':colorscheme gruvbox-material')
+
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', {noremap = true})
+-- telescop map
+vim.api.nvim_set_keymap('n', 'ff', ':Telescope find_files<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'fg', ':Telescope live_grep<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'fb', ':Telescope buffers<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'fh', ':Telescope help_tags<cr>', {noremap = true})
+
+
+
+require("plugins")
+
+-- ==============================================================================
+-- ==============================lspkind=========================================
+-- ==============================================================================
+require('lspkind').init({
+    -- DEPRECATED (use mode instead): enables text annotations
+    --
+    -- default: true
+    -- with_text = true,
+
+    -- defines how annotations are shown
+    -- default: symbol
+    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+    mode = 'symbol_text',
+
+    -- default symbol map
+    -- can be either 'default' (requires nerd-fonts font) or
+    -- 'codicons' for codicon preset (requires vscode-codicons font)
+    --
+    -- default: 'default'
+    preset = 'codicons',
+
+    -- override preset symbols
+    --
+    -- default: {}
+    symbol_map = {
+      Text = "",
+      Method = "",
+      Function = "",
+      Constructor = "",
+      Field = "ﰠ",
+      Variable = "",
+      Class = "ﴯ",
+      Interface = "",
+      Module = "",
+      Property = "ﰠ",
+      Unit = "塞",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "פּ",
+      Event = "",
+      Operator = "",
+      TypeParameter = ""
+    },
+})
+
+-- ==============================================================================
+-- ==============================lspkind=========================================
+-- ==============================================================================
+
+
+
+
+-- ===============================================================================
+-- =============================cmp setting ======================================
+-- ===============================================================================
+local cmp = require 'cmp'
+local lspkind = require('lspkind')
+
+
+cmp.setup {
+	window = {
+         completion = cmp.config.window.bordered(),
+         documentation = cmp.config.window.bordered(),
+  },
+
+	mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    }),
+
+	sources = {
+    	{ name = 'nvim_lsp' },
+    	{ name = 'luasnip' },
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
+  }
+}
+
+-- ===============================================================================
+-- =============================cmp setting ======================================
+-- ===============================================================================
+
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local lspconfig = require("lspconfig")
+
+
+
+-- auto start language server 
+local servers = {'sumneko_lua'}      -- lsp-server
+for _, lsp in ipairs(servers) do 
+	lspconfig[lsp].setup {}
+end
+
+
+require("mason").setup({       -- start mason config
+	ui = {
+		icons = {
+			package_installed = "",
+			package_pending = "",
+			package_uninstalled = "",
+		}
+	}
+
+})       
+require "lsp_signature".setup()
+
+require("nvim-autopairs").setup {}
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+   additional_vim_regex_highlighting = true,
+  },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  }
+}
+
+
+-- =====================================================
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "cpp"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+
+
+-- =====================================================================================================
+-- =====================================================================================================
+-- =====================================================================================================
+vim.opt.termguicolors = true
+require('bufferline').setup {
+        options = {
+                mode = "buffers",
+                number = "both",
+                indicator_icon = '▐',
+                buffer_close_icon = '★',
+                modified_icon = '●',
+                close_icon = '',
+                left_trunc_marker = '',
+                right_trunc_marker = '',
+                color_icons = true,
+                show_buffer_icons = true,
+                show_buffer_default_icons = true,
+                show_tab_indicators = true,
+                diagnostics = "nvim_lsp",
+        }
+}
+-- =.====================================================================================================
+-- =====================================================================================================
+-- =====================================================================================================
+
+
+require("nvim-tree").setup({
+	renderer = {
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└",
+            	edge = "│",
+            	item = "│",
+            	none = " ",
+			},
+		},
+	},
+})
+
+vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+local border = {
+      {"🭽", "FloatBorder"},
+      {"▔", "FloatBorder"},
+      {"🭾", "FloatBorder"},
+      {"▕", "FloatBorder"},
+      {"🭿", "FloatBorder"},
+      {"▁", "FloatBorder"},
+      {"🭼", "FloatBorder"},
+      {"▏", "FloatBorder"},
+}
+
+-- LSP settings (for overriding per client)
+local handlers =  {
+  ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
+  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
+}
+
+-- Do not forget to use the on_attach function
+--require 'lspconfig'.sumneko_lua.setup { handlers=handlers }
+
+-- To instead override globally
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+
+
+local signs = { Error = "", Warn = "∙", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+
+vim.diagnostic.config({
+  --virtual_text = {
+  --  prefix = '', -- Could be '●', '▎', 'x'
+  --}
+  virtual_text = false
+})
+
+
+-- symbol outline
+vim.g.symbols_outline = {
+    highlight_hovered_item = true,
+    show_guides = true,
+    auto_preview = true,
+    position = 'right',
+    relative_width = true,
+    width = 25,
+    auto_close = false,
+    show_numbers = false,
+    show_relative_numbers = false,
+    show_symbol_details = true,
+    preview_bg_highlight = 'Pmenu',
+    keymaps = { -- These keymaps can be a string or a table for multiple keys
+        close = {"<Esc>", "q"},
+        goto_location = "<Cr>",
+        focus_location = "o",
+        hover_symbol = "<C-space>",
+        toggle_preview = "K",
+        rename_symbol = "r",
+        code_actions = "a",
+    },
+    lsp_blacklist = {},
+    symbol_blacklist = {},
+    symbols = {
+        File = {icon = "", hl = "TSURI"},
+        Module = {icon = "", hl = "TSNamespace"},
+        Namespace = {icon = "", hl = "TSNamespace"},
+        Package = {icon = "", hl = "TSNamespace"},
+        Class = {icon = "𝓒", hl = "TSType"},
+        Method = {icon = "ƒ", hl = "TSMethod"},
+        Property = {icon = "", hl = "TSMethod"},
+        Field = {icon = "", hl = "TSField"},
+        Constructor = {icon = "", hl = "TSConstructor"},
+        Enum = {icon = "ℰ", hl = "TSType"},
+        Interface = {icon = "ﰮ", hl = "TSType"},
+        Function = {icon = "", hl = "TSFunction"},
+        Variable = {icon = "", hl = "TSConstant"},
+        Constant = {icon = "", hl = "TSConstant"},
+        String = {icon = "𝓐", hl = "TSString"},
+        Number = {icon = "#", hl = "TSNumber"},
+        Boolean = {icon = "⊨", hl = "TSBoolean"},
+        Array = {icon = "", hl = "TSConstant"},
+        Object = {icon = "⦿", hl = "TSType"},
+        Key = {icon = "🔐", hl = "TSType"},
+        Null = {icon = "NULL", hl = "TSType"},
+        EnumMember = {icon = "", hl = "TSField"},
+        Struct = {icon = "𝓢", hl = "TSType"},
+        Event = {icon = "🗲", hl = "TSType"},
+        Operator = {icon = "+", hl = "TSOperator"},
+        TypeParameter = {icon = "𝙏", hl = "TSParameter"}
+    }
+}
+
+
+-- toggle_preview 
+require("toggleterm").setup{
+	open_mapping = [[<c-\>]],
+	direction = 'float',
+	shade_terminals = false,
+	float_opts = {
+		border = 'curved'
+	}
+}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,1 +1,49 @@
 -- plugins.lua
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+    "neovim/nvim-lspconfig",
+  }
+
+   use { "williamboman/mason.nvim" }  -- lspconfig install mananger
+   use {'hrsh7th/nvim-cmp'}
+   use {'hrsh7th/cmp-nvim-lsp'}
+   use {'onsails/lspkind.nvim'}
+
+   use {"ray-x/lsp_signature.nvim"}
+
+   use {"sainnhe/gruvbox-material"} -- theme gruvbox
+
+   use {"nvim-treesitter/nvim-treesitter"}
+
+   use {"windwp/nvim-autopairs"}
+
+   use {"p00f/nvim-ts-rainbow"}
+
+   use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+
+   use {
+  		'kyazdani42/nvim-tree.lua',
+  	requires = {
+    		'kyazdani42/nvim-web-devicons', -- optional, for file icons
+  		},
+  	tag = 'nightly' -- optional, updated every week. (see issue #1193)
+	}
+
+	use {
+  		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+  		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use {'akinsho/toggleterm.nvim', tag = 'v2.*'}
+
+	use {'simrat39/symbols-outline.nvim'}
+
+	use {'rcarriga/nvim-notify'}
+
+  end)
