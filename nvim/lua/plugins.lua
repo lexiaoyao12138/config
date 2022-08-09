e17aa6f56e5c5cc95b23238d444998f10b37e0d0
lexiaoyao12138 <1639503985@qq.com>
2022-08-09 16:38:16 +0800

    update nvim plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,49 +1,57 @@
 -- plugins.lua
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  use {
-    "neovim/nvim-lspconfig",
-  }
+	use({
+		"neovim/nvim-lspconfig",
+	})
 
-   use { "williamboman/mason.nvim" }  -- lspconfig install mananger
-   use {'hrsh7th/nvim-cmp'}
-   use {'hrsh7th/cmp-nvim-lsp'}
-   use {'onsails/lspkind.nvim'}
+	use({ "williamboman/mason.nvim" }) -- lspconfig install mananger
+	use({ "hrsh7th/nvim-cmp" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "onsails/lspkind.nvim" })
 
-   use {"ray-x/lsp_signature.nvim"}
+	use({ "ray-x/lsp_signature.nvim" })
 
-   use {"sainnhe/gruvbox-material"} -- theme gruvbox
+	use({ "sainnhe/gruvbox-material" }) -- theme gruvbox
 
-   use {"nvim-treesitter/nvim-treesitter"}
+	use({ "nvim-treesitter/nvim-treesitter" })
 
-   use {"windwp/nvim-autopairs"}
+	use({ "windwp/nvim-autopairs" })
 
-   use {"p00f/nvim-ts-rainbow"}
+	use({ "p00f/nvim-ts-rainbow" })
 
-   use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
 
-   use {
-  		'kyazdani42/nvim-tree.lua',
-  	requires = {
-    		'kyazdani42/nvim-web-devicons', -- optional, for file icons
-  		},
-  	tag = 'nightly' -- optional, updated every week. (see issue #1193)
-	}
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icons
+		},
+		tag = "nightly", -- optional, updated every week. (see issue #1193)
+	})
 
-	use {
-  		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
 		-- or                            , branch = '0.1.x',
-  		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-	use {'akinsho/toggleterm.nvim', tag = 'v2.*'}
+	use({ "akinsho/toggleterm.nvim", tag = "v2.*" })
 
-	use {'simrat39/symbols-outline.nvim'}
+	use({ "simrat39/symbols-outline.nvim" })
 
-	use {'rcarriga/nvim-notify'}
+	use({ "rcarriga/nvim-notify" })
 
-  end)
+	use({ "lewis6991/gitsigns.nvim" })
+
+	use({ "mhartington/formatter.nvim" }) -- format code
+
+	use({ "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" })
+
+	use({ "NvChad/nvim-colorizer.lua" })
+end)
