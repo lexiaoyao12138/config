18ba40c6dc4fc46f63eb412389cb5b339cc3abe9
lexiaoyao12138 <1639503985@qq.com>
2022-10-15 21:22:48 +0800

    add some applications

--- linux/reg.sh
+++ linux/reg.sh
@@ -1,12 +1,8 @@
 # 获取当前路径
 path=$(pwd)
 
-# 修改源
-echo "修改源"
-ln -sf $path/sources.list /etc/apt/sources.list
-
 # 常用软件
-AppList=(git curl wget aria2 repgrep screenfetch sl egrep)
+AppList=(git curl wget aria2 screenfetch sl egrep ripgrep gcc g++ make cmake unzip)
 
 echo "下载常用软件..."
 for app in ${AppList[@]}
@@ -25,4 +21,3 @@ $(url)
 # 链接zsh配置文件
 echo "链接zsh配置文件"
 ln -sf $path/zshrc $HOME/.zshrc
-ln -sf $path/p10k.zsh $HOME/.p10k.zsh
