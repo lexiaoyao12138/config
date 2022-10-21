e36b1a91fa9c32e29e1622c563ca2d9fec73d715
lexiaoyao12138 <1639503985@qq.com>
2022-10-21 17:37:46 +0800

    modify theme config

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,12 +1,14 @@
-vim.g.gruvbox_material_background="hard"
-vim.g.gruvbox_material_foreground="mix"
+local vim = vim
+
+vim.g.gruvbox_material_background = "medium" -- hard medium soft
+vim.g.gruvbox_material_foreground = "original" -- material mix original
 -- vim.g.gruvbox_material_disable_italic_comment=1
-vim.g.gruvbox_material_enable_bold=1
-vim.g.gruvbox_material_enable_italic=0
-vim.g.gruvbox_material_diagnostic_text_highlight=1
-vim.g.gruvbox_material_diagnostic_text_highlight=1
-vim.g.gruvbox_material_diagnostic_line_highlight=1
-vim.g.gruvbox_material_statusline_style=1
-vim.g.gruvbox_material_lightline_disable_bold=1
+vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_enable_italic = 0
+vim.g.gruvbox_material_diagnostic_text_highlight = 1
+vim.g.gruvbox_material_diagnostic_text_highlight = 1
+vim.g.gruvbox_material_diagnostic_line_highlight = 1
+vim.g.gruvbox_material_statusline_style = 1
+vim.g.gruvbox_material_lightline_disable_bold = 1
 -- vim.g.gruvbox_material_current_word="italic"
 vim.api.nvim_command(":colorscheme gruvbox-material")
