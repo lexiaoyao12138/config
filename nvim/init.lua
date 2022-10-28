6bdb9db037ebeb246af773648fead9757b90dfb0
lexiaoyao12138 <1639503985@qq.com>
2022-10-28 11:22:11 +0800

    set float style

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,17 +65,14 @@ require("nvim-tree").setup({
   },
 })
 
-vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
-vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
-
 local border = {
-  { "┌", "FloatBorder" },
+  { "╭", "FloatBorder" },
   { "─", "FloatBorder" },
-  { "┐", "FloatBorder" },
+  { "╮", "FloatBorder" },
   { "│", "FloatBorder" },
-  { "┘", "FloatBorder" },
+  { "╯", "FloatBorder" },
   { "─", "FloatBorder" },
-  { "└", "FloatBorder" },
+  { "╰", "FloatBorder" },
   { "│", "FloatBorder" },
 }
 
