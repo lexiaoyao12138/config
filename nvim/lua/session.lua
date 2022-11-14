87afe3dace9cc7e0938ea9852c1f4b642c5675d8
lexiaoyao12138 <1639503985@qq.com>
2022-11-14 20:15:28 +0800

    session config

--- nvim/lua/session.lua
+++ nvim/lua/session.lua
@@ -1,17 +1,26 @@
 local vim = vim
 
-local ok, session = pcall(require, "auto-session")
+local ok, session = pcall(require, "session_manager")
 if not ok then
-	vim.notify("auto-session is not install!")
+	vim.notify("session is not install!")
 	return
 end
 
+local Path = require('plenary.path')
+session.setup({
+  sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
+  path_replacer = '__', -- The character to which the path separator will be replaced for session files.
+  colon_replacer = '++', -- The character to which the colon symbol will be replaced for session files.
+  autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+  autosave_last_session = true, -- Automatically save last session on exit and on session switch.
+  autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+  autosave_ignore_dirs = {}, -- A list of directories where the session will not be autosaved.
+  autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
+    'gitcommit',
+  },
+  autosave_ignore_buftypes = {}, -- All buffers of these bufer types will be closed before the session is saved.
+  autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
+  max_path_length = 80,  -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
+})
 
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
-session.setup {
-  log_level = "error",
-	auto_session_enabled = true,
-	auto_save_enabled = nil,
-	auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
-  auto_session_suppress_dirs = { "~/Projects", "~/Downloads" },
-}
+vim.keymap.set("n", ";s", ":Session load_session<CR>")
