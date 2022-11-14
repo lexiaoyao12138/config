cf7d67aea261a8d3600e6c4a82713f7d5f487ced
lexiaoyao12138 <1639503985@qq.com>
2022-11-14 20:14:06 +0800

    add hop config

---
+++ nvim/lua/hop_config.lua
@@ -0,0 +1,23 @@
+local vim = vim
+
+
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+vim.keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
+
+hop.setup { keys = 'etovxqpdygfblzhckisuran' }
+
+vim.keymap.set("n", "hc", "HopChar1<CR>")
+vim.keymap.set("n", "hl", "HopLineStart<CR>")
+vim.keymap.set("n", "ha", "HopAnywhere<CR>")
