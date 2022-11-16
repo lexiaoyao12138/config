b666d8f473fb60ff33f07ad083c8af02ef81b278
lexiaoyao12138 <1639503985@qq.com>
2022-11-16 14:19:05 +0800

    set lspsign doc

--- nvim/lua/lsp_config/lsp_sign.lua
+++ nvim/lua/lsp_config/lsp_sign.lua
@@ -1,6 +1,7 @@
 local cfg = {
-  floating_window = false,
-  -- toggle_key = "<c-g>",
+  floating_window = true,
+  toggle_key = "<c-g>",
+	close_timeout = 0,
   select_signature_key = "<c-n>",
   handler_opts = {
     border = "rounded" -- double, rounded, single, shadow, none
@@ -8,4 +9,4 @@ local cfg = {
 }
 
 -- function sign
-require("lsp_signature").setup(cfg)
+require("lsp_signature").setup(cfg, bufnr)
