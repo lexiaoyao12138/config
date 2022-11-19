6ae32d157556fdce012daa5d9f05fb94b21ad803
lexiaoyao12138 <1639503985@qq.com>
2022-11-19 20:35:55 +0800

    update

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -68,28 +68,26 @@ local vim = vim
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  light_style = "night", -- The theme is used when the background is set to light
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
   transparent = true, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
     -- Style to be applied to different syntax groups
     -- Value is any valid attr-list value for `:help nvim_set_hl`
     comments = { italic = true },
     keywords = { bold = true, italic = false },
-    functions = {bold = true},
+    functions = { bold = true, italic = true },
     -- variables = {italic = true},
     -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
+    -- sidebars = "transparent", -- style for sidebars, see below
+		Background = "transparent",
     sidebars = "transparent", -- style for sidebars, see below
+    floats = "transparent", -- style for floating windows
   },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.8, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  sidebars = { "qf", "help", "terminal"  }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.9, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-  hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
   dim_inactive = true, -- dims inactive windows
   lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 
@@ -101,8 +99,8 @@ require("tokyonight").setup({
   --- You can override specific highlights to use other groups or a hex color
   --- function will be called with a Highlights and ColorScheme table
   ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
 })
 
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd[[
+	colorscheme tokyonight
+]]
